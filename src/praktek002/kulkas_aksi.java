a140aa3691636219554b64b4a82952019d4e886e
Habib06 <habib06@gmail.com>
2018-04-01 15:12:05 +0800

    mengisi nilai atribute

--- src/praktek002/kulkas_aksi.java
+++ src/praktek002/kulkas_aksi.java
@@ -3,6 +3,9 @@ package praktek002;
 public class kulkas_aksi {
     public static void main(String[] args) {
          kulkas r1 = new kulkas();
+         
+         r1.harga_kulkas = 6000000;
+         r1.jumlah_kulkas = 5;  
     }
   
 }
